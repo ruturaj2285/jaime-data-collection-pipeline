0fd4c13dcf16b93c22072e703e8df47c4582e2ff
ruturaj2285 <ruturajgidde6460@gmail.com>
2025-12-03 14:38:33 +0530

    Implement AWS Lambda function to increment counter

---
+++ functions/check-status/app.py
@@ -0,0 +1,25 @@
+import boto3
+
+ssm = boto3.client("ssm")
+
+PARAM_NAME = "/counter/value"
+
+def lambda_handler(event, context):
+    # 1. Read current value
+    response = ssm.get_parameter(Name=PARAM_NAME)
+    current_value = int(response["Parameter"]["Value"])
+    
+    # 2. Increment
+    new_value = current_value + 1
+    
+    # 3. Print the number
+    print(f"Trigger Count: {new_value}")
+    
+    # 4. Save new value back to SSM
+    ssm.put_parameter(
+        Name=PARAM_NAME,
+        Value=str(new_value),
+        Overwrite=True
+    )
+    
+    return {"count": new_value}
